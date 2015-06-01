0affae13d9105ab1f4397d0ea782082f0c152a16
balajikvijayan <balaji.k.vijayan@gmail.com>
2015-06-01 16:26:46 -0700

    Spam Lord

--- week1/1.2/pa1-spamlord-v5/python/SpamLord.py
+++ week1/1.2/pa1-spamlord-v5/python/SpamLord.py
@@ -1,34 +1,95 @@
 import sys
 import os
-import re
+import regex as re
 import pprint
 
-my_first_pat = '(\w+)@(\w+).edu'
+pattern = r"[^//](\w+\.)?(\w+)(\s)?(at|@|&#x40;|WHERE){1}(\s)?(\w+)?(\s)?(\.|dot|;)?(\s)?(\w+)+(\s)?(\.|dot|dt|;|DOM)+(\s)?(edu|EDU|com{1})[^/|>| ]"
+pattern2 = r"[^//](\w+)(@){1}(\s{1})?(\w+)?(\.)(\w+)(.edu)[\[^/| |>]"
+pattern3 = r'(\w+.)?(\w+)?\s+(\(followed by &ldquo;|\(followed by ")(@)(\w+.?\w+)(.edu)'
+pattern4 =  r"[^//](\w+)[\s](at)[\s](\w+)(\s)(\w+)(\s)(edu)[,][\[^/| |>]"
+pattern5 = r"([\w-]+)(@)-([\w-]+).-([\w-]+\w)"
+obfuscatepattern = r"(\w+\.edu)','(\w+)"
+phone_pattern = r"\(?(\d{3})[\) -]+(\d{3})[ -]+(\d{4})"
 
-""" 
-TODO
-This function takes in a filename along with the file object (actually
-a StringIO object at submission time) and
-scans its contents against regex patterns. It returns a list of
-(filename, type, value) tuples where type is either an 'e' or a 'p'
-for e-mail or phone, and value is the formatted phone number or e-mail.
-The canonical formats are:
-     (name, 'p', '###-###-#####')
-     (name, 'e', 'someone@something')
-If the numbers you submit are formatted differently they will not
-match the gold answers
-
-NOTE: ***don't change this interface***
-"""
 def process_file(name, f):
-    # note that debug info should be printed to stderr
-    # sys.stderr.write('[process_file]\tprocessing file: %s\n' % (path))
+    """This really should be refactored to bring the pattern matching into its own function"""
+    """but I'm feeling lazy and this isn't production code so c'est le vie"""
     res = []
     for line in f:
-        matches = re.findall(my_first_pat,line)
+        matches = re.findall(pattern,line)
         for m in matches:
-            email = '%s@%s.edu' % m
+            email = ''
+            for x in m:
+                if x.strip() != '':
+                    if x == 'at' or x == '&#x40;' or x == 'WHERE':
+                        email += '@'
+                    elif x == 'dot' or x == 'dt' or x == ';' or x == 'DOM':
+                        email += '.'
+                    else:
+                        email+= x
             res.append((name,'e',email))
+        second_matches = re.findall(pattern2, line)
+        for m in second_matches:
+            email = ''
+            for x in m:
+                if x.strip() != '':
+                    if x == 'at' or x == '&#x40;' or x == 'WHERE':
+                        email += '@'
+                    elif x == 'dot' or x == 'dt' or x == ';' or x == 'DOM':
+                        email += '.'
+                    else:
+                        email+= x
+#             print email
+            res.append((name,'e',email))
+        third_matches = re.findall(pattern3, line)
+        for m in third_matches:
+            email = ''
+            for x in m:
+                if x.strip() != '':
+                    if x == '(followed by &ldquo;' or x == '(followed by "':
+                        email += ''
+                    elif x == 'dot' or x == 'dt' or x == ';' or x == 'DOM':
+                        email += '.'
+                    else:
+                        email+= x
+#             print m
+#             print type(m)
+            res.append((name,'e',email))
+        fourth_matches = re.findall(obfuscatepattern, line)
+        for m in fourth_matches:
+                email = '{0}@{1}'.format(m[1],m[0])
+#                 print email
+                res.append((name,'e',email))
+        matches4 = re.findall(pattern4,line)
+        for m in matches4:
+            email = ''
+            for x in m:
+            #      if x.strip() != '':
+                 if x == 'at':
+                     email += '@'
+                 elif x == ' ':
+                     email += '.'    
+                 else:
+                     email+= x
+            #print type(email)
+            res.append((name,'e',email))
+        fifth_matches = re.findall(pattern5, line)
+        email =''
+        for m in fifth_matches:
+            print m
+            for q in m:
+                email += q.replace('-','').replace('edu','.edu')
+        if email != '':
+            res.append((name,'e',email))
+        phone_matches = re.findall(phone_pattern,line)
+        for p in phone_matches:
+#             print p
+            phone = ''
+            for z in p:
+                if z != '(' and z != ')' and z != ' ' and z!= '-':
+                    phone += z
+#             print '{}-{}-{}'.format(phone[0:3],phone[3:6],phone[6:10])
+            res.append((name,'p','{}-{}-{}'.format(phone[0:3],phone[3:6],phone[6:10])))
     return res
 
 """
